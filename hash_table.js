3f62310c678ae62f1595bffe5aa457f7ca3a3010
Adesanmi Adeniyi <adesanmi.adeniyi@tellerium.io>
2022-12-26 11:34:28 +0100

    hash table

---
+++ hash_table.js
@@ -0,0 +1,42 @@
+class HashTable {
+    constructor (size){
+        this.data = new Array(size);
+    }
+
+    _hash(key){
+        let hash = 0;
+        for (let i = 0; i < key.length; i++) {
+            hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+    }
+        return hash;
+    }
+
+    set(key, value) {
+        let address = this._hash(key);
+        if (!this.data[address]){
+            this.data[address] = [];
+        }
+        this.data[address].push([key, value]);
+        return this.data
+    }
+
+    get(key) {
+        let address = this._hash(key);
+        const currentBucket = this.data[address];
+        console.log(currentBucket)
+        if(currentBucket){
+            for(let i = 0; i< currentBucket.length; i++) {
+                if(currentBucket[i][0] === key) {
+                    return console.log(currentBucket[i][1])
+                }
+            }
+            return undefined
+        }
+    }
+}
+
+const myHashTable = new HashTable(2)
+myHashTable.set('drinks', 500)
+myHashTable.set('banana', 230)
+myHashTable.set('pineapple', 74)
+myHashTable.get('banana')
